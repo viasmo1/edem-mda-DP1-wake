5e528cfce04e6ab62539e2e44be6b7dc66a86dc8
Vicent Asensio <vicent.asenmol@gmail.com>
2020-12-03 19:06:02 +0100

    update vuelo_directo.py

--- vuelo_directo.py
+++ vuelo_directo.py
@@ -33,86 +33,85 @@ ciudades.set_index("Index", inplace=True)
 print(ciudades)
 
 
-def city_code(city=""):
-    if city == "":
-        return "Invalid city name"
+def city_code(city):
+    # Connection to the API
+    conn = http.client.HTTPSConnection(
+        "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+    )
+    # Connection details
+    headers = {
+        "x-rapidapi-key": "3a09c225d7msh6c7ddddb42a1c0dp1e24b8jsn686ba0bd2b95",
+        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    }
+    # Request url
+    url = "/apiservices/autosuggest/v1.0/ES/EUR/en-UK/?query=" + city
+    # Connection request
+    conn.request(
+        "GET", url, headers=headers,
+    )
+
+    res = conn.getresponse()
+    data = res.read()
+    data = json.loads(data)
+
+    # Get the city code
+    if data["Places"] == []:
+        return False
     else:
-        # Connection to the API
-        conn = http.client.HTTPSConnection(
-            "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-        )
-        # Connection details
-        headers = {
-            "x-rapidapi-key": "3a09c225d7msh6c7ddddb42a1c0dp1e24b8jsn686ba0bd2b95",
-            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        }
-        # Request url
-        url = "/apiservices/autosuggest/v1.0/ES/EUR/en-UK/?query=" + city
-        # Connection request
-        conn.request(
-            "GET", url, headers=headers,
-        )
+        return data["Places"][0]["PlaceId"]
 
-        res = conn.getresponse()
-        data = res.read()
-        data = json.loads(data)
 
-        # Get the city code
-        if data["Places"] == []:
+def direct_route(destination_code):
+    # Connection to the API
+    conn = http.client.HTTPSConnection(
+        "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+    )
+    # Connection details
+    headers = {
+        "x-rapidapi-key": "3a09c225d7msh6c7ddddb42a1c0dp1e24b8jsn686ba0bd2b95",
+        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    }
+    # Destination city
+    url = (
+        "/apiservices/browsequotes/v1.0/ES/EUR/en-GB/VLC-sky/"
+        + destination_code
+        + "/anytime?inboundpartialdate=anytime"
+    )
+    # Connection request
+    conn.request(
+        "GET", url, headers=headers,
+    )
+
+    res = conn.getresponse()
+    data = res.read()
+    data = json.loads(data)
+
+    # Check if there is a direct flight
+    direct = False
+    for quote in data["Quotes"]:
+        if quote["Direct"] is True:
+            direct = True
+            break
+
+    return direct
+
+
+def direct_flight(city_name=""):
+    if city_name == "":
+        return "Empty city name"
+    else:
+        code = city_code(city_name)
+        if code == False:
             return False
         else:
-            return data["Places"][0]["PlaceId"]
-
-
-def direct_flight(destination_code="none"):
-    if destination_code == "none":
-        return "Invalid destination code"
-
-    elif destination_code == False:
-        return False
-
-    else:
-        # Connection to the API
-        conn = http.client.HTTPSConnection(
-            "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-        )
-        # Connection details
-        headers = {
-            "x-rapidapi-key": "3a09c225d7msh6c7ddddb42a1c0dp1e24b8jsn686ba0bd2b95",
-            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        }
-        # Destination city
-        url = (
-            "/apiservices/browsequotes/v1.0/ES/EUR/en-GB/VLC-sky/"
-            + destination_code
-            + "/anytime?inboundpartialdate=anytime"
-        )
-        # Connection request
-        conn.request(
-            "GET", url, headers=headers,
-        )
-
-        res = conn.getresponse()
-        data = res.read()
-        data = json.loads(data)
-
-        # Check if there is a direct flight
-        direct = False
-        for quote in data["Quotes"]:
-            if quote["Direct"] is True:
-                direct = True
-                break
-
-        return direct
+            return direct_route(code)
 
 
 # Calculate if there is direct flight
 for i in np.arange(0, len(ciudades)):
-    # Search city code
-    print(ciudades.Ciudad[i], ": Processed")
-    code = city_code(ciudades.Ciudad[i])
     # Search direct flight
-    ciudades["Vuelo_directo"][i] = direct_flight(code)
+    ciudades["Vuelo_directo"][i] = direct_flight(ciudades.Ciudad[i])
+    print(ciudades.Ciudad[i], ": Processed")
 
 
 print("RESULT:\n", ciudades)
