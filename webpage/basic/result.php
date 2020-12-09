937d6831d1b40436e747272f95fb09f39fd625b7
Vicent Asensio <vicent.asenmol@gmail.com>
2020-12-09 21:05:45 +0100

    test

--- webpage/basic/result.php
+++ webpage/basic/result.php
@@ -102,6 +102,7 @@ if ((($_POST["name"] != "" && $_POST["surname"] != "" && $_POST["age"] != "" && 
 
   // Save result in SESSION variable
   $_SESSION["id_city"] = $id_city;
+  $id_city = null;
 
   // Insert result into form table
   $date = date('d/m/Y h:i:s a', time());
