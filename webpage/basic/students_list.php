566a9b7436749ae69c3d7ccc61c0c57f14e4cd00
Vicent Asensio <vicent.asenmol@gmail.com>
2020-12-09 18:08:40 +0100

    update students_list to show results without city

--- webpage/basic/students_list.php
+++ webpage/basic/students_list.php
@@ -261,7 +261,7 @@ License: You must have a valid license purchased only from themeforest(the above
                             include_once "functions.php";
                             $db = "WakeTeam";
                             $conn = connect_db($db);
-                            $query = "SELECT student.id, student.name as st_name, student.age, student.email, city.name as c_name FROM student INNER JOIN form ON student.id=form.id_student INNER JOIN city ON city.id=form.id_city";
+                            $query = "SELECT student.id, student.name as st_name, student.age, student.email, city.name as c_name FROM student INNER JOIN form ON student.id=form.id_student LEFT JOIN city ON city.id=form.id_city";
                             $res = pg_query($conn, $query);
                             //Print student tablbe
                             while ($row = pg_fetch_assoc($res)) {
